3c7ec35e41e55b29634ce56e2e2831a30c1c4abf
Viraj Devruwan <rathnayaka.viraj@gmail.com>
2019-08-14 23:26:53 +0530

    Sayura Beach Hotel System first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Pages/login/login/login.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { SidebarComponent } from './Components/sidebar/sidebar.component';
+import { BookingComponent } from './Pages/booking/booking.component';
+import { CustomerUpdateComponent } from './Pages/customer-update/customer-update.component';
+import { CustomerDeleteComponent } from './Pages/customer-delete/customer-delete.component';
+import { UpdateBookingComponent } from './Pages/update-booking/update-booking.component';
+import { DeleteBookingComponent } from './Pages/delete-booking/delete-booking.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    SidebarComponent,
+    BookingComponent,
+    CustomerUpdateComponent,
+    CustomerDeleteComponent,
+    UpdateBookingComponent,
+    DeleteBookingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
