daa6a1d4ae16fb1bcc3ce7ce98c5a69661fd045d
MalinduD <malindudhananjana@gmail.com>
2019-08-21 13:08:09 +0800

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+<<<<<<< Updated upstream
 import { HomeComponent} from './Pages/customer/home/home.component';
 import { BookingComponent} from './Pages/customer/booking/booking.component';
 // tslint:disable-next-line:one-variable-per-declaration
@@ -7,6 +8,11 @@ const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'booking', component: BookingComponent}
 ];
+=======
+
+const routes: Routes = [];
+
+>>>>>>> Stashed changes
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './Pages/login/login/login.component';
 import { HomeComponent } from './Pages/customer/home/home.component';
 import { SidebarComponent } from './Components/sidebar/sidebar.component';
+<<<<<<< Updated upstream
 import { BookingComponent } from './Pages/customer/booking/booking.component';
 import { CustomerUpdateComponent } from './Pages/customer/customer-update/customer-update.component';
 import { CustomerDeleteComponent } from './Pages/customer/customer-delete/customer-delete.component';
@@ -14,6 +15,19 @@ import { UpdateBookingComponent } from './Pages/customer/update-booking/update-b
 import { DeleteBookingComponent } from './Pages/customer/delete-booking/delete-booking.component';
 import { HeaderComponent } from './Components/header/header/header.component';
 import { FooterComponent } from './Components/footer/footer.component';
+=======
+import { BookingComponent } from './Pages/booking/booking.component';
+import { CustomerUpdateComponent } from './Pages/customer-update/customer-update.component';
+import { CustomerDeleteComponent } from './Pages/customer-delete/customer-delete.component';
+import { UpdateBookingComponent } from './Pages/update-booking/update-booking.component';
+import { DeleteBookingComponent } from './Pages/delete-booking/delete-booking.component';
+import { ManageEmpDetailsComponent } from './Pages/Employee/Admin/manage-emp-details/manage-emp-details.component';
+import { UpdateEmpDetailsComponent } from './Pages/Employee/Admin/update-emp-details/update-emp-details.component';
+import { DeleteEmpDetailsComponent } from './Pages/Employee/Admin/delete-emp-details/delete-emp-details.component';
+import { AttendanceEmpDetailsComponent } from './Pages/Employee/Admin/attendance-emp-details/attendance-emp-details.component';
+import { WorkEmpDetailsComponent } from './Pages/Employee/Admin/work-emp-details/work-emp-details.component';
+import { EmpHomeComponent } from './Pages/Employee/Admin/emp-home/emp-home.component';
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
@@ -26,8 +40,17 @@ import { FooterComponent } from './Components/footer/footer.component';
     CustomerDeleteComponent,
     UpdateBookingComponent,
     DeleteBookingComponent,
+<<<<<<< Updated upstream
     HeaderComponent,
     FooterComponent
+=======
+    ManageEmpDetailsComponent,
+    UpdateEmpDetailsComponent,
+    DeleteEmpDetailsComponent,
+    AttendanceEmpDetailsComponent,
+    WorkEmpDetailsComponent,
+    EmpHomeComponent
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
