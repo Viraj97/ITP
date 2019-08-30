79ff5e31bc1ce3214abcd7dede68c4755c903566
NaminSaranga <naminsaranga54@gmail.com>
2019-08-30 14:33:02 +0530

    update
    update

--- src/app/Pages/Transport/update/update.component.ts
+++ src/app/Pages/Transport/update/update.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+
 declare const validate:any;
 
 @Component({
@@ -8,13 +10,36 @@ declare const validate:any;
 })
 export class UpdateComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient){}
+
+  model3 = {
+    id: '',
+    model: '',
+    license: '',
+    passengers: '',
+    year: '',
+    colour: '',
+
+  };
 
   ngOnInit() {
   }
 
-  onclick(){
-    validate();
-  }
+  // onclick(){
+  //   validate();
+  // }
+
+  submitted = false;
+  
+  onSubmit() {this.submitted = true;}
+
+method1(){
+  this.http.get('http://127.0.0.1:3000/AddVehicle',{
+    params: {VehicleID:this.model3.id,VehicleModel:this.model3.model,Licence:this.model3.license,Max:this.model3.passengers,
+      Year:this.model3.year,Colour:this.model3.colour}
+  }).subscribe((response)=>{
+    console.log('response',response[0].AddVehicle);
+  });
+}
   
 }
